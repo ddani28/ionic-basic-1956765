b14fa9e66e2d5bfc49fb519af10bf78be255617e
Daniel Chavez <dantoniochp@gmail.com>
2023-09-17 19:28:52 -0600

    Practica 4
    Crear dos paginas/componentes que se comunicaran entre si a traves de un servicio, consumir web service y ver la importancia de
    observable.subscribe observebable.unsubscribe

--- ionic-basic/src/app/alumnos/alumnos-routing.module.ts
+++ ionic-basic/src/app/alumnos/alumnos-routing.module.ts
@@ -7,7 +7,15 @@ const routes: Routes = [
   {
     path: '',
     component: AlumnosPage
-  }
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('../inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'receptor',
+    loadChildren: () => import('../receptor/receptor.module').then( m => m.ReceptorPageModule)
+  },
 ];
 
 @NgModule({
