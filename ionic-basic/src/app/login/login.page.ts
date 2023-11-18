a6683cd5e4fdfa907730334a9945fb38ef0f1855
Daniel Chavez <dantoniochp@gmail.com>
2023-11-18 16:03:33 -0600

    Practica 14
    spiner, loadController, cap android, cap ios

--- ionic-basic/src/app/login/login.page.ts
+++ ionic-basic/src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { ModalErrorComponent } from '../componentes/modal-error/modal-error.component';
-import { ModalController } from '@ionic/angular';
+import { ModalController, LoadingController } from '@ionic/angular';
 import { AutService } from '../service/aut.service';
 import { Router } from '@angular/router';
 import { MenuService } from '../service/menu.service';
@@ -19,7 +19,8 @@ export class LoginPage implements OnInit {
     private modalCtrl: ModalController,
     private autSvc: AutService,
     private menu: MenuService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    public loadingController: LoadingController
   ) { }
 
   ngOnInit() {
@@ -37,11 +38,15 @@ export class LoginPage implements OnInit {
     this.autSvc.onLogin(this.user).then((user:any)=>{
       if(user!=null && user.code ==undefined){
         console.log('Successfully logged in!');
-        this.menu.setTitle("presupuesto");
-        this.router.navigate(['main/presupuesto']);
+        this.loadingController.dismiss();
+        setTimeout(() => {
+          this.menu.setTitle("presupuesto");
+          this.router.navigate(['main/presupuesto']);
+        }, 650);
       }
       else{
         if(user.code){
+          this.loadingController.dismiss();
           if(user.code=='auth/wrong-password' || user.code =='auth/invalid-email' || user.code=='auth/argument-error'){
             this.openModal(user);
           }
@@ -77,6 +82,7 @@ export class LoginPage implements OnInit {
     if(this.ionicForm.valid){
       this.user.email = this.ionicForm.get('email').value;
       this.user.password = this.ionicForm.get('password').value;
+      this.presentLoadingWithOptions();
       this.onLogin();
     }
   } 
@@ -88,4 +94,20 @@ export class LoginPage implements OnInit {
     this.menu.setTitle("register")
     this.router.navigate(['/register']);
   }  
+  
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      //spinner: null,
+      duration: 5000,
+      message: 'Click the backdrop to dismiss early...',
+      //translucent: true,
+      //cssClass: 'custom-class custom-loading',
+      backdropDismiss: true
+    });
+
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed with role:', role);
+  }   
 }
